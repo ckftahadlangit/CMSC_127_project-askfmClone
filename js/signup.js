ac707d0c5d17c4f34a61c666387f73b1aa2e6f17
ckftahadlangit <cftahadlangit@up.edu.ph>
2017-12-21 00:25:26 +0800

    Added html and js files

---
+++ js/signup.js
@@ -0,0 +1,9 @@
+var config = {
+  apiKey: "AIzaSyCld-zlyyysUfAOw5CYj0Cn8w0QJAF0qPU",
+  authDomain: "cmsc127-926a8.firebaseapp.com",
+  databaseURL: "https://cmsc127-926a8.firebaseio.com",
+  projectId: "cmsc127-926a8",
+  storageBucket: "cmsc127-926a8.appspot.com",
+  messagingSenderId: "421673909250"
+};
+firebase.initializeApp(config);
